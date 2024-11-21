c1ac64ca6f828a004ff70d403ea8dfa0b642ce46
Farhaad <farhaad.sotoodeh@gmail.com>
2024-11-21 15:20:14 +0330

    5

--- cron/database.py
+++ cron/database.py
@@ -4,4 +4,4 @@ from pymongo import MongoClient
 
 MONGO_URI = 'mongodb://root:ea02910866ed4c0f9a982a489f606b76@mongo_db:27017/cronapi?authSource=admin'
 DB_NAME = MONGO_URI.split('/')[-1].split('?')[0]
-DB = MongoClient().get_database(DB_NAME)
+DB = MongoClient(MONGO_URI).get_database(DB_NAME)
